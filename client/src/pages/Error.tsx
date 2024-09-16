2f7bfe1baa3d5800769584d50c809d960c9293f1
Irfan Mahmood <irfanmahmood@Irfans-iMac.fios-router.home>
2024-09-16 10:34:32 -0400

    Styled 404 Page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,7 +1,26 @@
-const Error: React.FunctionComponent = () => {
-    return (
-        <h1>Error</h1>
-    );
+import {useRouteError, Link} from 'react-router-dom';
+import notFound from '../images/not_found.png';
+
+interface IError {
+    status: number
 }
+
+const Error: React.FunctionComponent = () => {
+    const error = useRouteError() as IError;
+    if (error.status === 404) {
+        return (
+        <div className="flex flex-col justify-center items-center text-center w-screen h-screen">
+            <img className="w-48 h-48 mb-4 rounded-full outline" src={notFound} alt="A chalkboard with some text saying 404 Not Found"/>
+            <p className="mb-2">Oopsies, looks like you don't know where you're going. How about home?</p>
+            <Link to="/" className="inline-block bg-white border border-black py-2 px-8 rounded-xl text-black no-underline hover:bg-black hover:text-white transition-colors duration-300">Back Home</Link>
+        </div>
+        );
+    }
+    return (
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <h1 className="text-2xl font-bold">Something went wrong, try again later!</h1>
+        </div>
+    );
+};
 
 export default Error;
